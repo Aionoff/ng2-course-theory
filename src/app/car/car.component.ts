d68848789a00f9153b6beb516791925ba84c637a
Aionoff <Aionoff62@mail.ru>
2019-08-05 14:27:13 +0300

    ng-content has been implemented

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -7,4 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CarComponent{
   @Input("carItem") car: {name: string, year: number}
+  @ContentChild("carHeading", {static: false}) carHeading: ElementRef;
+
+  ngAfterViewInit(){
+
+  }
 }
